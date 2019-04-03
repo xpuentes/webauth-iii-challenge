7f771381b47dc8b977b524e31064457bcc22407c
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-04-02 21:14:56 -0500

    Added POST /api/auth/register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
+const { jwtSecret } = require('../config/secrets.js');
 
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.register(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -1,5 +1,7 @@
 const db = require('../database/dbConfig');
 
 module.exports = {
-
+  register: (newUser) => {
+    return db('users').insert(newUser);
+  }
 }
